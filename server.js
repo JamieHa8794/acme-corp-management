20117f048f09764a6fc34c51c504e83c774288d9
JamieHa8794 <JamieHa8794@gmail.com>
2022-08-29 19:27:18 -0400

    set up express & display data

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-const {syncAndSeed } = require('./db/index')
+const {syncAndSeed, models: { User, Department} } = require('./db/index')
+const express = require('express')
+const app = express();
+
+app.get('/', async (req, res, next) =>{
+    try{
+        const [users, departments] = await Promise.all([
+            User.findAll(),
+            Department.findAll()
+        ])
+        res.send({
+            users,
+            departments
+        });
+    }
+    catch(err){
+        next(err);
+    }
+})
 
 
 const init = async () =>{
     try{
         await syncAndSeed();
-        console.log('connected to db');
+        const port = process.env.PORT || 3000;
+        app.listen(port, ()=> console.log(`listening on port ${port}`))
     }
     catch(err){
         console.log(err)
